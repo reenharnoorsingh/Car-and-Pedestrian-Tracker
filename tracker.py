20071e2000214be361b0ec81829a850fa96b77d7
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2020-09-13 15:08:35 +0530

    inital code

--- tracker.py
+++ tracker.py
@@ -1,3 +1,6 @@
 # Car and Pedestrian Tracker
 
 import cv2 #pip install opencv-python
+
+image_file = 'car.jpg' #car image check repository for the image
+classifier_file = 'car_detector.xml' #pre-trained car detection algorithm
